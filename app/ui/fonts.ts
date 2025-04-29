a4c20a2f0625ab3bb6ec249a67095ad351a4faa8
Wunna <wunnaaungsayargyi99@gmail.com>
2025-04-30 01:09:39 +0700

    font name changes

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import {Inter} from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});
-export const lustina = Lusitana({
+export const lusitana = Lusitana({
     subsets: ['latin'],
     weight: ['400', '700'],
 })
